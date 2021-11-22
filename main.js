0e070b917ddbd0a6765645806757a1a14d708148
Javad Moghaddam <sed.javadmoghaddam@gmail.com>
2021-11-22 23:34:13 +0330

    node number api and serving static assets

--- main.js
+++ main.js
@@ -13,14 +13,48 @@ const NODES = parse(nodeFilesFile);
 const app = express();
 app.use(cors());
 
+// to get file -> localhost:port/filename
+app.use(express.static("./owned/"));
+// app.use(express.static(CONFIG.owned_files_dir));
+
+function getNearestNode() {
+  let nearestNode = CONFIG.friend_nodes[0];
+  CONFIG.friend_nodes.forEach((node) => {
+    if (
+      Math.abs(node.node_name - nearestNode.node_name) <
+      Math.abs(CONFIG.node_name - nearestNode.node_name)
+    ) {
+      nearestNode = node;
+    }
+  });
+
+  return nearestNode;
+}
+
+app.get("/", (req, res) => res.send("Ok"));
+
+// example: /node?nodeNumber=3
 app.get("/node", (req, res) => {
   // if this node has the requested node number return
   // otherwise return node number closest to its number
-  res.send("Done");
-});
-
-app.get("/file", (req, res) => {
-  // return the requested file
+  const requestedNodeNumber = +req.query.nodeNumber;
+  const requestedNode = CONFIG.friend_nodes.find(
+    (node) => node.node_name === requestedNodeNumber
+  );
+  if (requestedNode)
+    res.send({
+      found: true,
+      nodeNumber: requestedNode.node_name,
+      port: requestedNode.node_port,
+    });
+  else {
+    const nearestNode = getNearestNode();
+    res.send({
+      found: false,
+      nodeNumber: nearestNode.node_name,
+      port: nearestNode.node_port,
+    });
+  }
 });
 
 app.listen(CONFIG["node_port"], () => {
@@ -29,45 +63,44 @@ app.listen(CONFIG["node_port"], () => {
       CONFIG["node_number"] +
       " started listening on port " +
       CONFIG["node_port"]
-  )
+  );
 
   listenForRequest();
 });
 
 function initialMessageValidate(message) {
-  if(message.request.split(" ").length != 2 || !message.request.includes("request")) {
+  if (
+    message.request.split(" ").length != 2 ||
+    !message.request.includes("request")
+  ) {
     return false;
-  }
-  else return true;
+  } else return true;
 }
 
-function getNode(fileName){
-
-  for(let node in NODES.node_files){
-    const isExist = NODES.node_files[node].node_files.find(file => file === fileName);
-    if(isExist === fileName){
+function getNode(fileName) {
+  for (let node in NODES.node_files) {
+    const isExist = NODES.node_files[node].node_files.find(
+      (file) => file === fileName
+    );
+    if (isExist === fileName) {
       return NODES.node_files[node].node_name;
     }
   }
 
   return null;
-
 }
-
 
 function onRequest(message) {
   // find the requested file by asking nodes
 
-  if(!initialMessageValidate(message)) { 
+  if (!initialMessageValidate(message)) {
     console.log("Invalid message");
-  }
-  else {
+  } else {
     let node = getNode(message.request.split(" ")[1]);
 
-    if(node === null){
+    if (node === null) {
       console.log("Not found this file");
-    }
-    else{
+    } else {
       console.log("Found this file in node " + node);
       //search for finding node
     }
